52532982f16c8c5a4582a460e878da7158746439
Dileepa Bandara <80202913+dileepadev@users.noreply.github.com>
2025-10-27 19:36:48 +0530

    feat: Add python_print

---
+++ 0_hello_world/python_print.py
@@ -0,0 +1,8 @@
+print("Hello, this is a simple message!")
+print(4+6)
+print("Calculations:", 4+6, 4-6, 4*6, 4/6)
+print("Calculations with labels:",
+      "| 4+6 =", 4+6,
+      "| 4-6 =", 4-6,
+      "| 4*6 =", 4*6,
+      "| 4/6 =", 4/6, "|")
