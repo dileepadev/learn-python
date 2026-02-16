dd3ad66c7d0f2eba6f21f07f0ee50071ea01a3ee
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-02-16 19:12:23 +0530

    feat(introduction): Add comments

---
+++ introduction/comments.py
@@ -0,0 +1,8 @@
+# Single-line comment in Python
+
+"""
+This is a multi-line comment in Python.
+It can span multiple lines and is often used for documentation.
+"""
+
+print("This line of code will be executed, while the comments above will be ignored.")
