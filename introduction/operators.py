261f821594910484764f3fce05f215b2a64daf06
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-02-25 20:37:50 +0530

    feat(introduction): Update operators

--- introduction/operators.py
+++ introduction/operators.py
@@ -3,15 +3,15 @@
 
 # Arithmetic Operators
 # Numbers
-a = 10
+a = 11
 b = 5
-print("Addition:", a + b)           # Output: 15
-print("Subtraction:", a - b)        # Output: 5
-print("Multiplication:", a * b)     # Output: 50
-print("Division:", a / b)           # Output: 2.0
+print("Addition:", a + b)           # Output: 16
+print("Subtraction:", a - b)        # Output: 6
+print("Multiplication:", a * b)     # Output: 55
+print("Division:", a / b)           # Output: 2.2
 print("Floor Division:", a // b)    # Output: 2
-print("Modulus:", a % b)            # Output: 0
-print("Exponentiation:", a ** b)    # Output: 100000
+print("Modulus:", a % b)            # Output: 1
+print("Exponentiation:", a ** b)    # Output: 161051
 
 # Strings
 str1 = "Hello"
