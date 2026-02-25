d46f8b7fdb5f495a5d7f1bbf54ddcbeafbdb339a
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-02-25 20:07:02 +0530

    feat(introduction): Update comments

--- introduction/comments.py
+++ introduction/comments.py
@@ -5,4 +5,9 @@ This is a multi-line comment in Python.
 It can span multiple lines and is often used for documentation.
 """
 
+'''
+This is another way to write multi-line comments in Python.
+It can also be used for documentation or to comment out blocks of code.
+'''
+
 print("This line of code will be executed, while the comments above will be ignored.")
