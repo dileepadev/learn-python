7247d45e2f223204ab121ed721dc54389cdf2083
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-03-04 21:44:12 +0530

    fix(introduction): Output comments of operators

--- introduction/operators.py
+++ introduction/operators.py
@@ -5,16 +5,16 @@
 # Numbers
 a = 11
 b = 5
-print("Addition:", a + b)           # Output: 16
-print("Subtraction:", a - b)        # Output: 6
-print("Multiplication:", a * b)     # Output: 55
-print("Division:", a / b)           # Output: 2.2
-print("Floor Division:", a // b)    # Output: 2
-print("Modulus:", a % b)            # Output: 1
-print("Exponentiation:", a ** b)    # Output: 161051
+print("Addition:", a + b)           # Output: Addition: 16
+print("Subtraction:", a - b)        # Output: Subtraction: 6
+print("Multiplication:", a * b)     # Output: Multiplication: 55
+print("Division:", a / b)           # Output: Division: 2.2
+print("Floor Division:", a // b)    # Output: Floor Division: 2
+print("Modulus:", a % b)            # Output: Modulus: 1
+print("Exponentiation:", a ** b)    # Output: Exponentiation: 161051
 
 # Strings
 str1 = "Hello"
 str2 = "World"
-print("String Concatenation:", str1 + " " + str2)  # Output: "Hello World"
-print("String Repetition:", str1 * 3)              # Output: "HelloHelloHello"
+print("String Concatenation:", str1 + " " + str2)  # Output: String Concatenation: Hello World
+print("String Repetition:", str1 * 3)              # Output: String Repetition: HelloHelloHello
